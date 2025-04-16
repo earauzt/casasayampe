3377709a5d5be205b3cc3628fc857e6473eade25
earauzt <earauzt@gmail.com>
2025-04-16 09:53:35 -0400

    Create analytics.js
    Add Google Analytics script

---
+++ s/analytics.js
@@ -0,0 +1,6 @@
+```javascript
+     window.dataLayer = window.dataLayer || [];
+     function gtag() { dataLayer.push(arguments); }
+     gtag('js', new Date());
+     gtag('config', 'G-8B688HM6RF');
+     ```
